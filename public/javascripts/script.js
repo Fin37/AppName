9087faac26d3670eb4ad8c52aba70fbe524187b0
Finley Vernon <finley.vernon37@gmail.com>
2020-09-25 15:48:31 +1200

    Updated profile view and made sure channels function correctly. Will soon work on the channel view to make it look more navigatable.

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -2,7 +2,3 @@ var loadFile = function(event) {
     var image = document.getElementById('output');
     image.src = URL.createObjectURL(event.target.files[0]);
 }
-
-function loadFile() {
-    document.getElementById("avatarlabel").innerHTML = "Picture Updated";
-}
